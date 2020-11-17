81c2f6afcf83a12ffa0b719605aa5c4473912ddc
Joni Lappalainen <joni.lappa@gmail.com>
2020-11-17 17:52:11 +0200

    Use parallel tool to run tasks in parallel

--- raw2jpg.sh
+++ raw2jpg.sh
@@ -3,14 +3,17 @@
 overwrite_jpg=false
 remove_raw=false
 
-usage() { echo "Usage: $0 -d <directory> [-f <remove_raw>] [-w <ovewrite_jpg>]" 1>&2; exit 1; }
+usage() { echo "Usage: $0 (-d <directory> | -f <rawfile>) [-r <remove_raw>] [-w <ovewrite_jpg>]" 1>&2; exit 1; }
 
-while getopts ":d:f:w:" o; do
+while getopts ":d:f:r:w:" o; do
     case "${o}" in
         d)
             path=${OPTARG}
             ;;
         f)
+            rawfile=${OPTARG}
+            ;;
+        r)
             remove_raw=${OPTARG}
             ;;
         w)
@@ -23,35 +26,42 @@ while getopts ":d:f:w:" o; do
 done
 shift $((OPTIND-1))
 
-if [ -z "${path}" ]; then
+if [ -z "${path}" ] && [ -z "${rawfile}" ] ; then
     usage
 fi
 
+
+
+if [ ! -z "${path}" ] ; then
 echo "path          = ${path}"
 echo "remove_raw    = ${remove_raw}"
 echo "overwrite_jpg = ${overwrite_jpg}"
 
-find $path -regextype posix-egrep -iregex '.*\.(rw2|NEF)' | while IFS= read -r raw_pathname; do
-    base=$(basename "$raw_pathname"); name=${base%.*}; ext=${base##*.}
+find $path -regextype posix-egrep -iregex '.*\.(rw2|NEF)' | parallel -j+0 "$0 -f {} -w $overwrite_jpg -r $remove_raw"
+
+elif [ ! -z "${rawfile}" ] ; then
+    base=$(basename "$rawfile"); name=${base%.*}; ext=${base##*.}
     jpg="${name}.jpg"
-    jpg_pathname=${raw_pathname//$base/$jpg}
+    jpg_pathname=${rawfile//$base/$jpg}
     changed=false
 
     echo "$base"
 
     if [ ! -e "$jpg_pathname" ] || ([ -e "$jpg_pathname" ] && [ $overwrite_jpg = true ]); then
             echo "    convert to ${jpg_pathname}"
-            dcraw -c -w "$raw_pathname" | convert - "$jpg_pathname"
-            exiftool -overwrite_original -tagsfromfile $raw_pathname -all:all $jpg_pathname
+            dcraw -c -w "$rawfile" | convert - "$jpg_pathname"
+            exiftool -overwrite_original -preserve -tagsfromfile $rawfile -all:all $jpg_pathname
             changed=true
     fi
     if [ -e "$jpg_pathname" ] && [ $remove_raw = true ] ; then
-        echo "    remove original $raw_pathname"
-        rm $raw_pathname
+        echo "    remove original $rawfile"
+        rm $rawfile
         changed=true
     fi
     if [ $changed = false ]; then
         echo "    nothing to do"
     fi
-done
+    
+fi
+
 
