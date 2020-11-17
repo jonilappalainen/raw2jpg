78f68da7945a98f9501036dbd53239aeea9fb0c5
Joni Lappalainen <joni.lappa@gmail.com>
2020-11-17 19:25:01 +0200

    Remove original rawfile only if the jpg exists and size is not zero.

--- raw2jpg.sh
+++ raw2jpg.sh
@@ -37,7 +37,7 @@ echo "path          = ${path}"
 echo "remove_raw    = ${remove_raw}"
 echo "overwrite_jpg = ${overwrite_jpg}"
 
-find $path -regextype posix-egrep -iregex '.*\.(rw2|NEF)' | parallel -j+0 "$0 -f {} -w $overwrite_jpg -r $remove_raw"
+find $path -regextype posix-egrep -iregex '.*\.(rw2|NEF)' | parallel -j2 "$0 -f {} -w $overwrite_jpg -r $remove_raw"
 
 elif [ ! -z "${rawfile}" ] ; then
     base=$(basename "$rawfile"); name=${base%.*}; ext=${base##*.}
@@ -48,20 +48,18 @@ elif [ ! -z "${rawfile}" ] ; then
     echo "$base"
 
     if [ ! -e "$jpg_pathname" ] || ([ -e "$jpg_pathname" ] && [ $overwrite_jpg = true ]); then
-            echo "    convert to ${jpg_pathname}"
-            dcraw -c -w "$rawfile" | convert - "$jpg_pathname"
-            exiftool -overwrite_original -preserve -tagsfromfile $rawfile -all:all $jpg_pathname
-            changed=true
-    fi
-    if [ -e "$jpg_pathname" ] && [ $remove_raw = true ] ; then
-        echo "    remove original $rawfile"
-        rm $rawfile
+        echo "    convert to ${jpg_pathname}"
+        dcraw -c -w "$rawfile" | convert - "$jpg_pathname"
+        exiftool -overwrite_original -tagsfromfile $rawfile -all:all $jpg_pathname
         changed=true
+        if [ -s "$jpg_pathname" ] && [ $remove_raw = true ]; then$
+            echo "    remove original $rawfile"$
+            rm $rawfile$
+        fi$
     fi
     if [ $changed = false ]; then
         echo "    nothing to do"
     fi
-    
 fi
 
 
